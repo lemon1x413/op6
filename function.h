264abdd0bf135da2ec161e65be02225b4cab7ec0
lemon1x413 <romalemontop@gmail.com>
2024-12-09 16:10:51 +0200

    final?

--- function.h
+++ function.h
@@ -1,23 +1,33 @@
 #ifndef OP6_FUNCTION_H
 #define OP6_FUNCTION_H
 
-void freeArrays(int n, double *x, double *b, double *xp, double **a) {
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define BLUE "\x1b[36m"
+#define YELLOW  "\x1b[33m"
+#define RESET "\033[0m"
+
+void freeArrays(int n, double **a, double *xp, double *x, double *b) {
     for (int i = 0; i < n; i++) {
         free(a[i]);
     }
-    free(x);
-    free(b);
-    free(xp);
     free(a);
+    free(x);
+    free(xp);
+    free(b);
 }
 
 int validInputInt(char *message, int min, int max) {
     int value = 0, validInput = 0;
     do {
+        printf(GREEN);
         printf("%s", message);
+        printf(RESET);
         validInput = scanf("%d", &value);
         if (validInput != 1 || value < min || value > max) {
+            printf(RED);
             printf("Invalid input\n");
+            printf(RESET);
         }
         fflush(stdin);
     } while (validInput != 1 || value < min || value > max);
@@ -28,10 +38,14 @@ double validInputDouble(char *message, double min, double max) {
     double value = 0;
     int validInput = 0;
     do {
+        printf(GREEN);
         printf("%s", message);
+        printf(RESET);
         validInput = scanf("%lf", &value);
         if (validInput != 1 || value < min || value > max) {
+            printf(RED);
             printf("Invalid input\n");
+            printf(RESET);
         }
         fflush(stdin);
     } while (validInput != 1 || value < min || value > max);
@@ -41,10 +55,14 @@ double validInputDouble(char *message, double min, double max) {
 int validInputChoice(char *message, int choice1, int choice2) {
     int value = 0;
     do {
+        printf(GREEN);
         printf("%s", message);
+        printf(RESET);
         value = getch();
         if (value != choice1 && value != choice2) {
+            printf(RED);
             printf("Invalid input\n");
+            printf(RESET);
         }
         fflush(stdin);
     } while (value != choice1 && value != choice2);
@@ -57,22 +75,10 @@ double random(double min, double max) {
     return min + (rand() / div);
 }
 
-void randomMatrixFilling(int size, int min, int max, double **a, double *b) {
-    double nonDiagonalElementSum = 0;
-    for (int i = 0; i < size; i++) {
-        nonDiagonalElementSum = 0.0;
-        for (int j = 0; j < size; j++) {
-            if (i != j) {
-                a[i][j] = random(min, max);
-                nonDiagonalElementSum += fabs(a[i][j]);
-            }
-            a[i][i] = nonDiagonalElementSum + 1.0;
-        }
-        b[i] = random(min, max);
-    }
-}
-
 void manualMatrixFilling(int size, int min, int max, double **a, double *b) {
+    printf(YELLOW);
+    printf("Input matrix must be diagonally dominant (sum of non diagonal elements must be less the diagonal element in each row)\n");
+    printf(RESET);
     double diagonalElement = 0, nonDiagonalElementSum = 0;
     for (int i = 0; i < size; i++) {
         diagonalElement = 0.0;
@@ -95,7 +101,9 @@ void manualMatrixFilling(int size, int min, int max, double **a, double *b) {
                 }
             }
             if (diagonalElement <= nonDiagonalElementSum) {
-                printf("Sum of non diagonal elements must be less then diagonal element\n");
+                printf(RED);
+                printf("Input matrix isn`t diagonally dominant.\n");
+                printf(RESET);
             }
         } while (diagonalElement <= nonDiagonalElementSum);
         printf("Enter b%d ", i + 1);
@@ -103,7 +111,22 @@ void manualMatrixFilling(int size, int min, int max, double **a, double *b) {
     }
 }
 
-void calculateSlae(double **a, double *b, double *xp, double *x, int n, double epsilon) {
+void randomMatrixFilling(int size, int min, int max, double **a, double *b) {
+    double nonDiagonalElementSum = 0;
+    for (int i = 0; i < size; i++) {
+        nonDiagonalElementSum = 0.0;
+        for (int j = 0; j < size; j++) {
+            if (i != j) {
+                a[i][j] = random(min, max);
+                nonDiagonalElementSum += fabs(a[i][j]);
+            }
+            a[i][i] = nonDiagonalElementSum + random(1.0, 10.0);
+        }
+        b[i] = random(min, max);
+    }
+}
+
+void calculateSlae(double **a, const double *b, double *xp, double *x, int n, double epsilon) {
     double sum = 0.0, delta = 0.0, deltaMax = 0.0;
     do {
         deltaMax = 0.0;
@@ -127,24 +150,25 @@ void calculateSlae(double **a, double *b, double *xp, double *x, int n, double e
     } while (epsilon < deltaMax);
 }
 
-void printSlae(unsigned rows, double **a, double *b, unsigned precision) {
-    printf("\nInitial SLAE: \n\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < rows; j++) {
-            if (j == 0) {
-                printf(" %.*lf*x%d", precision, a[i][j], j + 1);
-            } else {
-                printf(" %+.*lf*x%d", precision, a[i][j], j + 1);
-            }
-        }
-        printf(" = %.*lf\n", precision, b[i]);
-    }
-}
-
 void printElements(double *arr, int rows, unsigned precision) {
     for (int i = 0; i < rows; i++) {
+        printf(BLUE);
         printf("x[%02d] = %.*lf\n", i + 1, precision, arr[i]);
+        printf(RESET);
     }
 }
 
+void print_init_slae(double **a, const double *b, const int size) {
+    printf("Initial SLAE:\n");
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if(j == 0) {
+                printf(" %.1lf * x%d", a[i][j], j+1);
+            } else {
+                printf(" %+.1lf * x%d", a[i][j], j+1);
+            }
+        }
+        printf(" = %.1lf\n", b[i]);
+    }
+}
 #endif
--- main.c
+++ main.c
@@ -13,9 +13,18 @@
 #define MAX_EPS 1e-1
 #define ENTER 13
 
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define BLUE "\x1b[36m"
+#define YELLOW  "\x1b[33m"
+#define RESET "\033[0m"
+
 int main() {
     srand(time(NULL));
     do {
+        printf(YELLOW);
+        printf("This program solves SLAE\n");
+        printf(RESET);
         int size = validInputInt("Enter size of SLAE (from 2 to 100):\n", MIN_SIZE, MAX_SIZE);
         double epsilon = validInputDouble("Enter precision of calculations (from 1e-15 to 1e-1):\n", MIN_EPS, MAX_EPS);
         unsigned precision = (unsigned) fabs(log10(epsilon));
@@ -26,19 +35,16 @@ int main() {
         for (int i = 0; i < size; i++) {
             a[i] = calloc(size, sizeof(double));
             if (a[i] == NULL) {
-                printf("Memory allocation error!\n");
-                freeArrays(size, x, b, xp, a);
-                getch();
+                freeArrays(size, a, xp, x, b);
                 return 0;
             }
         }
         if (x == NULL || b == NULL || xp == NULL || a == NULL) {
-            printf("Memory allocation error!\n");
-            freeArrays(size, x, b, xp, a);
-            getch();
+            freeArrays(size, a, xp, x, b);
             return 0;
         }
-        int choiceMatrixFilling = validInputChoice("Choose filling method (1 - manual input, 2 - random generated)\n", '1', '2');
+        int choiceMatrixFilling = validInputChoice("Choose filling method (1 - manual input, 2 - random generated)\n",
+                                                   '1', '2');
         switch (choiceMatrixFilling) {
             case '1':
                 manualMatrixFilling(size, MIN, MAX, a, b);
@@ -52,9 +58,11 @@ int main() {
         }
         calculateSlae(a, b, xp, x, size, epsilon);
         printElements(x, size, precision);
-        printSlae(size, a, b, precision);
-        freeArrays(size, x, b, xp, a);
+        print_init_slae(a, b, size);
+        freeArrays(size, a, xp, x, b);
+        printf(YELLOW);
         printf("Press ENTER to continue, or any other key to exit.\n");
+        printf(RESET);
     } while (getch() == ENTER);
     return 0;
 }
